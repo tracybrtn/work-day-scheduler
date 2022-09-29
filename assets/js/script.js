3ec457682e977fcb1eea77c61cfa5301ce3df31d
tracybrtn <tracynburton@gmail.com>
2022-09-28 21:51:04 -0500

    color-changing timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,62 @@ $(document).ready(function () {
         console.log("button clicked");
         
         //get nearby values in jquery
-        console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        console.log(time);
-        console.log(text);
-
         // set time and text on local storage
         localStorage.setItem(time,text);
+    });
 
-    })
-});
-    
-    //check what time it is
-    // if actual time is more than timeblock then give past attributes
-    // if present time equals timeblock then give presen attribute
-    // if present time is less than time block then give timeblock attribute of future
+    function timeTracker() {
+        //check what time it is
+        var currentTime = moment().hour()
+        console.log(currentTime);
 
+        //loop
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("00"))
+            console.log(timeBlock)
+            console.log(currentTime)
 
+            //Add or remove classes depending on time
 
-    //when i reload the page items from local storage are pulled down
+            // if current time is greater than time block then give it past class
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+
+            // if current time is equal than time block then give it present class
+            } else if (timeBlock === currentTime) {
+                $(this).removeClass("future");
+                $(this).addClass("present");
+                $(this).removeClass("past");
+
+            // if current time is lesser than time block then give it future class
+            } else if (timeBlock > currentTime) {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+
+            // will leave this here since moment.js is now legacy
+            } else {
+                console.log('time tracker error');
+            }
+        })
+    }
+
+    //get items from local storage, if any
+    $("#0900 .description").val(localStorage.getItem("0900"));
+    $("#1000 .description").val(localStorage.getItem("1000"));
+    $("#1100 .description").val(localStorage.getItem("1100"));
+    $("#1200 .description").val(localStorage.getItem("1200"));
+    $("#1300 .description").val(localStorage.getItem("1300"));
+    $("#1400 .description").val(localStorage.getItem("1400"));
+    $("#1500 .description").val(localStorage.getItem("1500"));
+    $("#1600 .description").val(localStorage.getItem("1600"));
+    $("#1700 .description").val(localStorage.getItem("1700"));
+
+    //Call time tracker function after everything has been loaded
+    timeTracker();
+})
